2e5879c7f71154ad8178186656a7219bfee7fa45
achrefbenammar404 <achref.benammar404@gmail.com>
2025-04-10 17:55:25 +0100

    add: import FileUploadModule in CvModule for file upload integration

--- src/cv/cv.module.ts
+++ src/cv/cv.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CvService } from './cv.service';
 import { CvController } from './cv.controller';
-
+import { FileUploadModule } from 'src/file-upload/file-upload.module';
 @Module({
   controllers: [CvController],
   providers: [CvService],
+  imports: [FileUploadModule],
 })
 export class CvModule {}
