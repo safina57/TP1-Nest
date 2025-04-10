0f4e7f493d0007b7844036cb35632289463f7f60
achrefbenammar404 <achref.benammar404@gmail.com>
2025-04-10 18:04:31 +0100

    fix: ensure file presence check in ImageValidationPipe

--- src/file-upload/pipes/image_validation.pipe.ts
+++ src/file-upload/pipes/image_validation.pipe.ts
@@ -5,7 +5,10 @@ export class ImageValidationPipe implements PipeTransform {
   transform(value: Express.Multer.File) {
     const allowedTypes = ['image/jpeg', 'image/png', 'image/jpg'];
     const maxSize = 1 * 1024 * 1024; 
-
+    if (!value) {
+      throw new BadRequestException('File is required.');
+    }
+    
     if (!allowedTypes.includes(value.mimetype)) {
       throw new BadRequestException('File type not allowed. Only .jpg, .jpeg, and .png are accepted.');
     }
