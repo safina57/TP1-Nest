a3e9e807c6e2a6cb0fb3f01be1c632ff7d2fb46b
RedRoss1 <oussama.kraiem@insat.ucar.tn>
2025-04-10 19:01:34 +0100

    resolve merge conflict @achrefbenammar404

--- src/cv/cv.module.ts
+++ src/cv/cv.module.ts
@@ -1,7 +1,8 @@
-// src/cv/cv.module.ts
+import { FileUploadModule } from 'src/file-upload/file-upload.module';
 import { Module } from '@nestjs/common';
 import { CvController } from './cv.controller';
-
+import { CvService } from './cv.service'; 
+import { PrismaService } from 'src/prisma/prisma.service';
 @Module({
   controllers: [CvController],
   providers: [CvService, PrismaService],
