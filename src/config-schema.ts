ab7195de91b01b4f9439733a8ad00b75df10ab17
achrefbenammar404 <achref.benammar404@gmail.com>
2025-05-02 18:05:54 +0100

    feat: add Redis configuration options to the schema

--- src/config-schema.ts
+++ src/config-schema.ts
@@ -5,4 +5,6 @@ export const configSchema = Joi.object({
   SALT_ROUNDS: Joi.string().default(14),
   JWT_SECRET: Joi.string().required(),
   JWT_EXPIRATION: Joi.number().default(3600),
+  REDIS_HOST: Joi.string().default('localhost'),
+  REDIS_PORT: Joi.number().default(6379),
 });
