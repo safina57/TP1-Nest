a8cc02a04e4a917cad8973099d1d15079c03722c
RedRoss1 <oussama.kraiem@insat.ucar.tn>
2025-05-14 15:02:28 +0100

    add CvEvent

---
+++ prisma/migrations/20250514140123_add_cv_event/migration.sql
@@ -0,0 +1,20 @@
+-- CreateEnum
+CREATE TYPE "CvEventType" AS ENUM ('CREATE', 'UPDATE', 'DELETE');
+
+-- CreateTable
+CREATE TABLE "CvEvent" (
+    "id" TEXT NOT NULL,
+    "type" "CvEventType" NOT NULL,
+    "timestamp" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" TEXT NOT NULL,
+    "cvId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "CvEvent_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "CvEvent" ADD CONSTRAINT "CvEvent_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CvEvent" ADD CONSTRAINT "CvEvent_cvId_fkey" FOREIGN KEY ("cvId") REFERENCES "Cv"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
